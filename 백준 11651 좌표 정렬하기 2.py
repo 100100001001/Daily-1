91ccabc5e1d7d0b83a4166076a1aa5d823c9a65f
100100001001 <86655689+100100001001@users.noreply.github.com>
2021-10-22 20:56:19 +0900

    Create 백준 11651 좌표 정렬하기 2.py

---
+++ 백준 11651 좌표 정렬하기 2.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/2751
+
+import sys
+input = sys.stdin.readline
+
+coordination = [ input().split() for _ in range(int(input())) ]
+coordination = sorted(coordination, key=lambda x: [int(x[1]), int(x[0])])
+
+for i in coordination:
+    for j in i:
+        print(int(j), end=' ')
+    print(end='\n')
