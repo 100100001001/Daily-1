875c0f4ee673f1cab3691de00e6c0363a6a156c7
100100001001 <86655689+100100001001@users.noreply.github.com>
2021-11-17 23:33:58 +0900

    Create 백준 17614 369.py

---
+++ 백준 17614 369.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/17614
+
+n = int(input())
+cnt = 0
+
+for i in range(1, n+1):
+    i = str(i)
+
+    cnt += i.count('3') + i.count('6') + i.count('9')
+    
+print(cnt)
