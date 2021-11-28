28c8407c9b0e7045de10885c75d617f698e33704
100100001001 <86655689+100100001001@users.noreply.github.com>
2021-11-28 20:51:43 +0900

    Create 백준 17388 와글와글 숭고한.py

---
+++ 백준 17388 와글와글 숭고한.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/17388
+
+S, K, H = map(int, input().split())
+ 
+if S + K + H >= 100:
+    print("OK")
+else:
+    if min(S, K, H) == S:
+        print("Soongsil")
+    elif min(S, K, H) == K:
+        print("Korea")
+    else:
+        print("Hanyang")
