40f0ad0db36d5c485880b4aa8e197e26ef3f018d
100100001001 <ppangkko@gmail.com>
2022-05-20 11:09:19 +0900

    Create 백준 1225 이상한 곱셈.py

---
+++ 백준 1225 이상한 곱셈.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/1225
+
+# import sys
+# input = sys.stdin.readline
+
+# a, b = input().split()
+# add = 0
+
+# for i in a:
+#     for j in b:
+#         add += int(i) * int(j)
+
+# print(add)
+
+'''
+1×3 + 1×4 + 2×3 + 2×4 + 1×3 + 1×4 = 28
+(1+2+1)*(3+4) = 28
+'''
+
+import sys
+input = sys.stdin.readline
+
+a, b = input().split()
+
+a = list(map(int, a))
+b = list(map(int, b))
+
+print(sum(a) * sum(b))
+
